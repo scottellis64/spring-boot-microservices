145ade7045c51c4cf291e4c79c8ca1ea80aaf508
scottellis64 <j.scott.ellis64@gmail.com>
2023-10-20 13:32:39 -0400

    Added kafka services that work in one direction

---
+++ services/kafka/sample-kafka-consumer/src/main/java/com/sellis/samplekafkaconsumer/SampleKafkaConsumerApplication.java
@@ -0,0 +1,11 @@
+package com.sellis.samplekafkaconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleKafkaConsumerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SampleKafkaConsumerApplication.class, args);
+    }
+}
